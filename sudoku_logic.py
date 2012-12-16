5ec7c097ff52787282740e45103d9926b9e2f247
Ville Salonen <ville.salonen@iki.fi>
2012-12-16 14:42:48 +0200

    Solver can now solve medium sudokus.

--- sudoku_logic.py
+++ sudoku_logic.py
@@ -1,5 +1,6 @@
 from sudoku_utils import *
 
+
 def _basic_elimination(table):
     rows = [511] * 9
     cols = [511] * 9
@@ -33,11 +34,50 @@ def _basic_elimination(table):
 
     return table
 
+
 def _no_definite_value_in_cell(cell):
     return count_set_bits(cell) != 1
 
+
 def _locked_value_in_cell(cell):
     return count_set_bits(cell) == 1
+
+
+def _lock_last_occurrences(table):
+    previous_bits = [0] * 9
+    possible_bits = [0] * 9
+
+    # FIRST PASS: Find all possible values per box.
+    for cell_ix in xrange(0, 81):
+        box_ix = get_box_ix(cell_ix);
+
+        # Step 1: What values in current cell have been found previously in this box? (previous_values & current_value)
+        previously_used = previous_bits[box_ix] & table[cell_ix]
+
+        # Step 2: Calculate used bits in current value by possible bits and previously used bits
+        used_bits = possible_bits[box_ix] & previously_used
+
+        # Step 3: Remove used bits from possible bits
+        possible_bits[box_ix] ^= used_bits
+
+        # Step 4: Find new bits from current value
+        new_bits = table[cell_ix] ^ previously_used
+
+        # Step 5: Add new bits to possible bits
+        possible_bits[box_ix] ^= new_bits
+
+        # Step 6: Add current bits to previously found bits
+        previous_bits[box_ix] |= table[cell_ix]
+
+
+    # SECOND PASS: Lock values if only one occurrence is found per box.
+    for cell_ix in xrange(0, 81):
+        box_ix = get_box_ix(cell_ix)
+        if table[cell_ix] & possible_bits[box_ix]:
+            table[cell_ix] &= possible_bits[box_ix]
+
+    return table
+
 
 def solve(table):
     solved_before = count_solved_numbers(table)
@@ -48,6 +88,7 @@ def solve(table):
         solved_before = solved_after
 
         table = _basic_elimination(table)
+        table = _lock_last_occurrences(table)
         print "Step %d: %s" % (step, str(table))
 
         solved_after = count_solved_numbers(table)
--- test_sudoku.py
+++ test_sudoku.py
@@ -17,3 +17,19 @@ class SudokuTests(unittest.TestCase):
         expected_table = convert_human_table_to_bit_table(expected_table)
         easy_input = "004702000002190356600083000108057000090040030000320508000270003981036700000805400"
         self.assertEqual(expected_table, solve_input(easy_input))
+
+    def test_solve__medium_input__returns_solved(self):
+        expected_table = [2, 5, 9,  6, 4, 1,  3, 7, 8,
+                          7, 6, 4,  2, 8, 3,  1, 5, 9,
+                          8, 3, 1,  9, 5, 7,  6, 4, 2,
+
+                          9, 4, 6,  8, 3, 2,  5, 1, 7,
+                          1, 2, 3,  5, 7, 6,  8, 9, 4,
+                          5, 8, 7,  4, 1, 9,  2, 3, 6,
+
+                          3, 9, 8,  1, 2, 4,  7, 6, 5,
+                          4, 7, 2,  3, 6, 5,  9, 8, 1,
+                          6, 1, 5,  7, 9, 8,  4, 2, 3]
+        expected_table = convert_human_table_to_bit_table(expected_table)
+        easy_input = "259000000000003009000957040940000507120000094507000036090124000400300000000000423"
+        self.assertEqual(expected_table, solve_input(easy_input))
